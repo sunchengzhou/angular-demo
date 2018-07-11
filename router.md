476bdf74eb706feebafa410e45b3fad0ef188068
sunchengzhou <scz20160220@163.com>
2018-07-12 00:29:01 +0800

    add file

---
+++ router.md
@@ -0,0 +1,13 @@
+# angular Route导航
+
+| 名称 | 简介 |
+| - | :-: |
+| Routes | 路由的配置信息|
+| RouterOutlet | 路由的展示容器 |
+| Router | 路由执行对象，可以用其navigate(),navigateByUrl()跳转指定路由 |
+| RouterLink | html中与a标签配合的跳转 |
+| ActivatedRoute | 激活的路由对象，保存了路由地址，路由参数等 |
+
+
+
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { ProductComponent } from './product/product.component';
 import { Code404Component } from './code404/code404.component'
 
 const routes: Routes = [
+  // 这里不用/,支持相对、绝对路径
   {path:'', component: HomeComponent},
   {path:'product', component: ProductComponent},
+  //  通配符路由放到最后面
   {path:'**', component: Code404Component}
 ];
 
