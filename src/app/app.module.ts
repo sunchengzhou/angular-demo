5c582f2377b543df45a33e5cd7a54359a82a9842
sunchengzhou013 <sunchengzhou013@lu.com>
2018-07-17 12:35:59 +0800

    add communicate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { Code404Component } from './code404/code404.component';
-import { BindComponent } from './bind/bind.component';
 import { FormsModule } from '@angular/forms';
-import { MultipipePipe } from './pipes/multipipe.pipe';
 
 
 @NgModule({
   declarations: [   //组件  指令 管道
     AppComponent,
-    Code404Component,
-    BindComponent,
-    MultipipePipe,
   ],
   imports: [
     BrowserModule,  //  浏览器模块
--- src/app/pipes/multipipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MultipipePipe } from './multipipe.pipe';
-
-describe('MultipipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MultipipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
